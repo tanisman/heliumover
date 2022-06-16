71b86463cacf2fb273282efbc59266d7893eb73c
tanisman <mail@sezercantanisman.com.tr>
2022-06-16 20:07:36 +0300

    enqueue_push_data to enqueue_upstream_msg fix

--- proxy/listener.py
+++ proxy/listener.py
@@ -18,9 +18,9 @@ def lora_upstream(json_object):
         http_status, respose = heliumover_api.post_upstream(helium_api.MY_HOTSPOT["address"], json_object)
         if http_status != 200:
             logging.warning(f"[heliumover] post upstream returned {http_status} ({respose})")
-        downstream_client.enqueue_push_data(json.dumps({"rxpk": [json_object]}))
+        downstream_client.enqueue_upstream_msg(json.dumps({"rxpk": [json_object]}))
     else:
-        downstream_client.enqueue_push_data(json.dumps(json_object), True)
+        downstream_client.enqueue_upstream_msg(json.dumps(json_object), True)
 
 def lora_downstream(json_object):
     global mutex
